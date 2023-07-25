cba73755fecf603a0c513f8578593830fadce29e
Ferry Ananda Febian <ferry.a.febian@gmail.com>
2023-07-25 23:31:35 +0800

    fitur analisa per assessment

---
+++ src/components/Assessment.ts
@@ -0,0 +1,147 @@
+export const assessment1 = [
+  "null",
+  "Apakah organisasi telah mengembangkan kebijakan keamanan informasi yang sesuai dengan kebutuhan dan persyaratan yang dijelaskan dalam ISO 27002?",
+  "Bagaimana kebijakan keamanan informasi organisasi dikembangkan agar sesuai dengan karakteristik, risiko, dan kebutuhan unik organisasi?",
+  "Apakah kebijakan keamanan informasi yang telah dikembangkan oleh organisasi terkait dan sejalan dengan kebijakan lainnya yang ada?",
+  "Sejauh mana kebijakan keamanan informasi telah diterapkan secara konsisten di seluruh organisasi?",
+  "Bagaimana organisasi memantau dan mengukur kepatuhan terhadap kebijakan keamanan informasi yang telah dikembangkan?",
+  "Apakah dilakukan audit internal terhadap implementasi, kepatuhan, dan efektivitas kebijakan keamanan informasi?",
+  "Bagaimana tinjauan manajemen dilakukan untuk memastikan kebijakan keamanan informasi tetap relevan dan sesuai dengan perubahan lingkungan bisnis dan kebutuhan organisasi?,",
+];
+export const assessment2 = [
+  "null",
+  "Apakah organisasi memiliki struktur organisasi yang ditetapkan secara jelas untuk mengelola dan bertanggung jawab atas keamanan informasi?",
+  "Apakah struktur organisasi keamanan informasi dirancang dan diimplementasikan dengan baik dalam organisasi?",
+  "Apakah peran dan tanggung jawab dalam pengelolaan keamanan informasi telah ditetapkan dengan jelas?",
+  "Apakah ada unit atau departemen khusus yang fokus pada keamanan informasi dan memiliki kewenangan yang memadai di dalam organisasi?",
+  "Apakah terdapat kolaborasi yang baik antara tim keamanan informasi dan unit lain dalam organisasi, seperti manajemen senior, departemen TI, dan bagian operasional?",
+  "Apakah organisasi memiliki proses dan mekanisme yang efektif untuk melaporkan, menangani, dan mengelola insiden keamanan informasi?",
+  "Apakah personel yang ditugaskan untuk mengelola keamanan informasi memiliki pengetahuan, keterampilan, dan pelatihan yang diperlukan untuk melaksanakan tugas mereka?",
+];
+export const assessment3 = [
+  "null",
+  "Apakah organisasi memiliki proses yang ditetapkan untuk mengidentifikasi, mengklasifikasikan, dan mengelola aset informasi yang dimiliki?",
+  "Apakah ada kebijakan dan prosedur yang jelas untuk melindungi aset informasi organisasi, termasuk tindakan pengamanan yang tepat?",
+  "Apakah aset informasi organisasi telah diidentifikasi dengan jelas, termasuk kepemilikan dan tanggung jawab atas aset tersebut?",
+  "Apakah organisasi memiliki langkah-langkah untuk memastikan perlindungan aset informasi dari ancaman dan risiko yang ada?",
+  "Apakah ada prosedur yang ditetapkan untuk memelihara dan memantau kondisi serta keamanan aset informasi organisasi?",
+  "Apakah terdapat kebijakan atau prosedur yang mengatur pemindahan, penghapusan, atau pemusnahan aset informasi yang tidak lagi diperlukan atau relevan?",
+  "Apakah organisasi memiliki mekanisme untuk mendeteksi, melaporkan, dan menangani kehilangan atau penyalahgunaan aset informasi?",
+];
+export const assessment4 = [
+  "null",
+  "Apakah organisasi memiliki program atau kebijakan yang secara khusus dirancang untuk meningkatkan kesadaran keamanan informasi di kalangan karyawan?",
+  "Apakah karyawan menerima pelatihan keamanan informasi yang tepat dan berkala, termasuk penekanan pada praktik keamanan yang relevan dengan tugas dan tanggung jawab mereka?",
+  "Apakah terdapat kebijakan atau prosedur yang memastikan bahwa karyawan memahami dan mematuhi praktik keamanan informasi yang telah ditetapkan?",
+  "Apakah organisasi memiliki mekanisme untuk melibatkan karyawan dalam identifikasi, penilaian, dan pengurangan risiko keamanan informasi?",
+  "Apakah terdapat insentif atau penghargaan yang diberikan kepada karyawan yang berkontribusi secara positif dalam praktik keamanan informasi?",
+  "Apakah organisasi memiliki prosedur yang efektif untuk mengelola perubahan peran, mutasi, atau pemutusan hubungan kerja dengan karyawan dalam konteks keamanan informasi?",
+  "Apakah terdapat mekanisme untuk melaporkan pelanggaran keamanan informasi dan apakah karyawan merasa nyaman melaporkannya?",
+];
+export const assessment5 = [
+  "null",
+  "Apakah organisasi memiliki kebijakan dan prosedur yang jelas terkait dengan kontrol akses fisik dan logis untuk melindungi informasi yang dimiliki?",
+  "Apakah terdapat pengaturan fisik yang memadai, seperti penguncian pintu, pengawasan area terbatas, dan penggunaan kartu akses, untuk mencegah akses fisik yang tidak sah ke ruang dan fasilitas yang mengandung informasi sensitif?",
+  "Apakah terdapat mekanisme untuk mengelola dan mengontrol hak akses pengguna terhadap sistem informasi yang berbasis logis?",
+  "Apakah prosedur autentikasi yang kuat, seperti penggunaan kata sandi yang kompleks, multi-faktor, atau biometrik, diterapkan untuk mengamankan akses logis terhadap informasi?",
+  "Apakah organisasi melakukan pengawasan dan pemantauan terhadap aktivitas akses fisik dan logis untuk mendeteksi dan mengatasi upaya akses yang tidak sah?",
+  "Apakah ada kebijakan dan prosedur yang ditetapkan untuk mengelola perubahan terkait hak akses fisik dan logis, termasuk pengangkatan hak akses yang tidak lagi diperlukan?",
+  "Apakah organisasi memiliki proses untuk melakukan audit terhadap kontrol akses fisik dan logis secara berkala guna memastikan kepatuhan dan keefektifan implementasinya?",
+];
+export const assessment6 = [
+  "null",
+  "Apakah organisasi telah mengembangkan rencana pemulihan bencana yang memadai untuk mengatasi gangguan layanan yang mungkin terjadi?",
+  "Apakah proses identifikasi risiko dan analisis dampak telah dilakukan untuk menyusun rencana pemulihan bencana yang tepat dan relevan?",
+  "Apakah rencana pemulihan bencana mencakup langkah-langkah yang jelas untuk memulihkan layanan dengan cepat dan efisien setelah terjadinya gangguan?",
+  "Apakah ada mekanisme untuk menguji dan melatih rencana pemulihan bencana secara berkala guna memastikan keandalan dan kesiapan saat diimplementasikan?",
+  "Apakah rencana pemulihan bencana diperbarui secara teratur sesuai dengan perubahan yang terjadi dalam infrastruktur teknologi dan kebutuhan organisasi?",
+  "Apakah peran dan tanggung jawab individu atau tim yang bertanggung jawab dalam pelaksanaan rencana pemulihan bencana telah ditetapkan dengan jelas?",
+  "Apakah ada mekanisme pelaporan dan evaluasi setelah terjadinya gangguan yang memungkinkan organisasi untuk mengidentifikasi pelajaran dan melakukan perbaikan berkelanjutan pada rencana pemulihan bencana?",
+];
+export const assessment7 = [
+  "null",
+  "Apakah organisasi memiliki proses pemantauan yang efektif untuk mengawasi operasi keamanan dan mendeteksi potensi ancaman atau pelanggaran keamanan?",
+  "Apakah ada jadwal dan prosedur pemeliharaan yang ditetapkan untuk memastikan keberlanjutan dan kinerja optimal sistem keamanan?",
+  "Apakah sistem keamanan organisasi dilindungi secara memadai dari serangan dan gangguan dengan menggunakan teknologi dan solusi keamanan yang sesuai?",
+  "Apakah telah ditetapkan kebijakan dan prosedur untuk menjaga kerahasiaan, integritas, dan ketersediaan data dan sistem?",
+  "Apakah organisasi memiliki prosedur pemulihan bencana yang memadai untuk mengatasi kerusakan atau gangguan yang dapat terjadi pada sistem keamanan operasional?",
+  "Apakah dilakukan penilaian risiko secara berkala terhadap sistem keamanan operasional guna mengidentifikasi ancaman baru dan memastikan langkah-langkah perlindungan yang relevan?",
+  "Apakah organisasi memiliki mekanisme pelaporan dan penanganan insiden yang efektif untuk mengatasi dan merespons kejadian keamanan yang mungkin terjadi?",
+];
+
+export const assessment8 = [
+  "null",
+  "Apakah ada kebijakan dan prosedur yang telah ditetapkan untuk melindungi informasi saat diproses, disimpan, dan ditransmisikan?",
+  "Apakah dilakukan enkripsi untuk melindungi kerahasiaan dan integritas informasi yang diproses, disimpan, atau ditransmisikan melalui jaringan atau media lainnya?",
+  "Apakah ada mekanisme pengelolaan kunci yang aman untuk mendukung implementasi enkripsi dan menghindari penggunaan kunci yang lemah atau tidak aman?",
+  "Apakah terdapat pemantauan dan pengendalian yang efektif untuk melindungi informasi saat diproses, seperti kontrol akses, pemisahan tugas, dan pembatasan hak akses?",
+  "Apakah langkah-langkah keamanan teknis telah diimplementasikan untuk melindungi informasi saat disimpan, seperti penggunaan firewall, antivirus, dan mekanisme keamanan lainnya?",
+  "Apakah ada kebijakan dan prosedur yang mengatur penggunaan perangkat seluler, perangkat USB, dan media penyimpanan lainnya untuk mencegah kebocoran atau penyalahgunaan informasi?",
+  "Apakah ada langkah-langkah perlindungan yang efektif untuk melindungi informasi saat ditransmisikan melalui jaringan, seperti penggunaan VPN, protokol keamanan, atau tautan aman?",
+];
+
+export const assessment9 = [
+  "null",
+  "Apakah ada kebijakan dan prosedur yang jelas untuk mengelola akses ke sistem informasi?",
+  "Apakah kebijakan dan prosedur tersebut dikomunikasikan kepada semua pengguna sistem informasi?",
+  "Apakah ada proses untuk memverifikasi identitas pengguna sebelum mereka diberikan akses ke sistem informasi?",
+  "Apakah ada proses untuk membatasi akses pengguna ke data dan sistem yang mereka butuhkan untuk melakukan pekerjaan mereka?",
+  "Apakah ada proses untuk melacak akses pengguna ke sistem informasi dan data?",
+  "Apakah ada proses untuk menanggapi akses yang tidak sah atau tidak pantas ke sistem informasi?",
+  "Apakah ada proses untuk memulihkan sistem informasi dari akses yang tidak sah atau tidak pantas?",
+];
+
+export const assessment10 = [
+  "null",
+  "Apakah ada kebijakan dan prosedur yang jelas untuk integrasi keamanan dalam siklus hidup pengembangan sistem informasi?",
+  "Apakah kebijakan dan prosedur tersebut dikomunikasikan kepada semua pihak yang terlibat dalam pengembangan sistem informasi?",
+  "Apakah ada proses untuk mengidentifikasi dan mengevaluasi risiko keamanan pada sistem informasi yang sedang dikembangkan?",
+  "Apakah ada proses untuk mengurangi risiko keamanan pada sistem informasi yang sedang dikembangkan?",
+  "Apakah ada proses untuk menguji keamanan sistem informasi yang sedang dikembangkan?",
+  "Apakah ada proses untuk mendokumentasikan keamanan sistem informasi yang sedang dikembangkan?",
+  "Apakah ada proses untuk memelihara keamanan sistem informasi yang telah dikembangkan?",
+];
+
+export const assessment11 = [
+  "null",
+  "Apakah organisasi memiliki kebijakan dan prosedur yang ditetapkan untuk mengelola insiden keamanan dan pelanggaran keamanan yang terjadi?",
+  "Apakah terdapat mekanisme pelaporan yang efektif untuk melaporkan insiden keamanan dan pelanggaran keamanan kepada pihak yang berwenang?",
+  "Apakah organisasi memiliki tim atau personil yang ditugaskan secara khusus untuk menangani dan merespons insiden keamanan dengan cepat dan efektif?",
+  "Apakah ada proses investigasi yang ditetapkan untuk menganalisis penyebab dan dampak insiden keamanan serta untuk mengambil tindakan yang sesuai?",
+  "Apakah dilakukan dokumentasi dan pelaporan yang tepat terkait dengan insiden keamanan dan langkah-langkah yang diambil untuk menanganinya?",
+  "Apakah organisasi melakukan pelatihan dan kesadaran kepada karyawan terkait dengan pelaporan insiden keamanan dan pentingnya kerjasama dalam penanganannya?",
+  "Apakah terdapat prosedur pemulihan setelah terjadinya insiden keamanan untuk mengembalikan sistem dan data ke keadaan yang aman dan operasional?",
+];
+
+export const assessment12 = [
+  "null",
+  "Apakah organisasi memiliki kebijakan dan prosedur yang mengatur keamanan informasi saat menjalin hubungan bisnis dengan pihak eksternal?",
+  "Apakah dilakukan evaluasi risiko terkait dengan hubungan bisnis dengan pihak eksternal untuk memastikan perlindungan informasi dan kepatuhan terhadap standar keamanan yang berlaku?",
+  "Apakah terdapat perjanjian kerahasiaan atau kontrak keamanan yang ditetapkan dengan pihak eksternal untuk melindungi informasi rahasia atau sensitif?",
+  "Apakah ada mekanisme pengawasan dan pemantauan yang efektif untuk mengontrol akses dan penggunaan informasi oleh pihak eksternal?",
+  "Apakah terdapat proses pengendalian untuk memastikan bahwa informasi yang dibagikan kepada pihak eksternal hanya sesuai dengan kebutuhan bisnis dan sesuai dengan tingkat keamanan yang telah ditentukan?",
+  "Apakah dilakukan pemantauan terhadap kepatuhan pihak eksternal terhadap persyaratan keamanan informasi yang telah ditetapkan?",
+  "Apakah organisasi memiliki mekanisme untuk menangani pelanggaran keamanan yang dilakukan oleh pihak eksternal dan mengambil tindakan yang sesuai?",
+];
+
+export const assessment13 = [
+  null,
+  "Apakah organisasi memiliki proses yang ditetapkan untuk memastikan kepatuhan terhadap persyaratan hukum yang berlaku terkait dengan keamanan informasi?",
+  "Apakah dilakukan evaluasi terhadap regulasi dan persyaratan industri yang berlaku untuk memastikan kepatuhan dan implementasi yang tepat?",
+  "Apakah ada tim atau personil yang ditugaskan secara khusus untuk memantau dan mengelola kepatuhan terhadap persyaratan hukum, regulasi, dan standar industri yang relevan?",
+  "Apakah terdapat kebijakan dan prosedur yang telah ditetapkan untuk mengelola perubahan dalam persyaratan hukum atau regulasi terkait keamanan informasi?",
+  "Apakah dilakukan audit internal secara berkala untuk mengevaluasi tingkat kepatuhan organisasi terhadap persyaratan hukum, regulasi, dan standar industri yang berlaku?",
+  "Apakah terdapat proses pelaporan yang efektif untuk melaporkan pelanggaran hukum, regulasi, atau persyaratan industri terkait keamanan informasi?",
+  "Apakah organisasi memiliki mekanisme pemantauan dan peninjauan yang sistematis untuk memastikan bahwa kepatuhan dengan persyaratan hukum dan regulasi terus dipatuhi?",
+];
+
+export const assessment14 = [
+  null,
+  "Apakah organisasi melakukan audit keamanan informasi secara teratur untuk mengevaluasi efektivitas kontrol keamanan yang telah diimplementasikan?",
+  "Apakah dilakukan penjadwalan audit keamanan informasi yang mencakup seluruh aspek sistem dan kebijakan keamanan yang ada?",
+  "Apakah ada tim atau personel yang bertanggung jawab secara khusus dalam melaksanakan audit keamanan informasi di organisasi?",
+  "Apakah terdapat metode dan prosedur audit yang telah ditetapkan untuk memastikan konsistensi dan obyektivitas dalam pelaksanaan audit keamanan informasi?",
+  "Apakah hasil dari audit keamanan informasi dikomunikasikan secara efektif kepada pihak yang berwenang dan pihak-pihak yang terkait?",
+  "Apakah organisasi telah mengambil langkah-langkah untuk menindaklanjuti rekomendasi atau temuan dari audit keamanan informasi yang telah dilakukan sebelumnya?",
+  "Apakah dilakukan pemantauan dan pengukuran terhadap tindak lanjut yang diambil sebagai hasil dari audit keamanan informasi untuk memastikan implementasi yang efektif?",
+];
